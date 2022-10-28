b8d561a7079e72bf136c19578e2b29dc332d75e7
meliliit <lilijena84@gmail.com>
2022-10-28 12:05:40 +0530

    wrong code

---
+++ palindroem.js
@@ -0,0 +1,14 @@
+
+let str="sadsa";
+let bag1="";
+let bag2="";
+for(let i=0;i<=str.length-1;i++){
+bag1=bag1+str(i);
+}
+for(let j=str.length-1;j>=0;j--){
+bag2=bag2+str(i);
+}
+if(bag1==bag2){
+console.log("palindrome");
+}
+//add else condition
